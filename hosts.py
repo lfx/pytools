82b854733e82e90bf9c28c2b65c6e2baa1ce41f1
Liudas Sodonis <liudas@sodonis.lt>
2011-05-16 15:54:23 +0300

    God I hate ident...

---
+++ hosts.py
@@ -0,0 +1,10 @@
+import commands
+
+def hosts():
+	for i in range (1,254):
+		x = commands.getoutput('host 192.168.0.'+str(i)).split(' ')[-1]
+		if x != '3(NXDOMAIN)':
+				print i,x
+
+if __name__ == '__main__':
+	hosts()  
